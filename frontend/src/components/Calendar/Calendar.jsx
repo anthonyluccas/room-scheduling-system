1e56b9100e5b47dc063b03ae34a71790d133a6dd
Anthony Luccas <anthonyferreira.dev@gmail.com>
2026-04-20 13:14:20 -0300

    feat: add calendar component

---
+++ frontend/src/components/Calendar/Calendar.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
+function Calendar() {
+  const [mes, setMes] = useState(4);
+  const [ano, setAno] = useState(2026);
+  const meses = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ];
+  const primeiroDia = new Date(ano, mes - 1, 1).getDay();
+  const totalDias = new Date(ano, mes, 0).getDate();
+  const dias = [
+    ...Array(primeiroDia).fill(null),
+    ...Array.from({ length: totalDias }, (_, i) => i + 1),
+  ];
+
+  return (
+    <div className="p-6 text-white">
+      <div className="flex items-center gap-4 mb-6 justify-between">
+        <button
+          onClick={() => {
+            if (mes === 1) {
+              setMes(12);
+              setAno(ano - 1);
+            } else {
+              setMes(mes - 1);
+            }
+          }}
+        >
+          <ChevronLeft />
+        </button>
+
+        <p>
+          {meses[mes - 1]} / {ano}
+        </p>
+
+        <button
+          onClick={() => {
+            if (mes === 12) {
+              setMes(1);
+              setAno(ano + 1);
+            } else {
+              setMes(mes + 1);
+            }
+          }}
+        >
+          <ChevronRight />
+        </button>
+      </div>
+
+      <div>
+        <div className="grid grid-cols-7 mb-2">
+          {["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"].map((dia) => (
+            <div
+              key={dia}
+              className="text-center text-sm text-gray-400 font-bold p-4"
+            >
+              {dia}
+            </div>
+          ))}
+
+          {dias.map((dia, index) => (
+            <div
+              key={index}
+              className="border border-gray-800 p-2 min-h-16 text-sm"
+            >
+              {dia}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Calendar;
--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,20 @@
 import Sidebar from "@/components/Sidebar/Sidebar";
+import Calendar from "@/components/Calendar/Calendar";
 
 function Dashboard() {
   return (
     <div className="flex h-screen bg-[#0f1117]">
       <Sidebar />
-      <div className="flex-1"></div>
+
+      <div className="flex-1">
+        <header className="flex items-center justify-between p-6 border-b border-gray-800">
+          <h2 className="text-white font-bold text-2xl">Calendario</h2>
+          <button className="mr-6 bg-[#6366f1] text-white font-bold p-3 rounded-lg hover:bg-[#4f46e5] transition cursor-pointer">
+            Novo Agendamento
+          </button>
+        </header>
+        <Calendar />
+      </div>
     </div>
   );
 }
